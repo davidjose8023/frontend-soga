c1bb7e11fd35a1372f715a15ae3bcc3c00c6d83f
David <davidjose8023@gmail.com>
2020-02-06 18:48:20 -0300

    validacion formulario

--- src/app/pages/pacientes/paciente.component.ts
+++ src/app/pages/pacientes/paciente.component.ts
@@ -15,26 +15,71 @@ export class PacienteComponent implements OnInit {
   imagenSubir: File;
   imagenTemp: any;
   forma : FormGroup;
+  nombresValidado: any = {has_danger:'', form_control_danger:'', msj:''};
+  apellidosValidado: any = {display:'', msj:''};
+  rutValidado: any = {display:'', msj:''};
+  correolValidado: any = {display:'', msj:''};
+  telefonoValidado: any = {display:'', msj:''};
+ 
+ 
+
  
   constructor(private titulo_navegador: Title) { }
-  validar(campo1: string){
+  validar(campo1: string, campo2: string, campo3: string, campo4:string, campo5: string){
   
     return (group: FormGroup) => {
       let nombre = group.controls[campo1].value;
+      let apellido = group.controls[campo2].value;
+      let correo = group.controls[campo3].value;
+      let rut = group.controls[campo4].value;
+      let telefono = group.controls[campo5].value;
      
-      
+  
       if(nombre){
-          if(nombre.length < 2){
-
-            return null;
-          }
-
-        
-      }
-      if(!nombre){
-
+    
         return null;
+    
+      }else{
+        
+        this.nombresValidado.msj= 'El nombre es requerido';
+        this.nombresValidado.has_danger = `form-group has-danger`;
+        this.nombresValidado.form_control_danger = `form-control has-danger`;
       }
+
+      if(apellido){
+    
+        return null;
+    
+      }else{
+        this.apellidosValidado.display= 'block';
+        this.apellidosValidado.msj= 'El apellido es requerido';
+      }     
+
+      if(correo){
+    
+        return null;
+    
+      }else{
+        this.correolValidado.display= 'block';
+        this.correolValidado.msj= 'El correo es requerido';
+      }
+      if(rut){
+    
+        return null;
+    
+      }else{
+        this.rutValidado.display= 'block';
+        this.rutValidado.msj= 'El rut es requerido';
+      }
+      if(telefono){
+    
+        return null;
+    
+      }else{
+        this.telefonoValidado.display= 'block';
+        this.telefonoValidado.msj= 'El telefono es requerido';
+      }
+     
 
       return {
         validacion: true
@@ -50,7 +95,7 @@ export class PacienteComponent implements OnInit {
       correo: new FormControl(null, [Validators.email]),
       rut: new FormControl(null, Validators.required),
       telefono: new FormControl(null, Validators.required)
-    }, this.validar('nombres'));
+    }, this.validar('nombres','apellidos','correo','rut','telefono'));
   }
 
   
