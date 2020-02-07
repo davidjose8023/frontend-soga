8e32277454e85e4432bcc8309004093deb4a26cb
davidjose8023 <davidjose8023@gmail.com>
2020-02-07 00:36:03 -0300

    validaciones formulario

--- src/app/pages/pacientes/paciente.component.ts
+++ src/app/pages/pacientes/paciente.component.ts
@@ -15,88 +15,98 @@ export class PacienteComponent implements OnInit {
   imagenSubir: File;
   imagenTemp: any;
   forma : FormGroup;
+ 
   nombresValidado: any = {has_danger:'', form_control_danger:'', msj:''};
-  apellidosValidado: any = {display:'', msj:''};
-  rutValidado: any = {display:'', msj:''};
-  correolValidado: any = {display:'', msj:''};
-  telefonoValidado: any = {display:'', msj:''};
+  apellidosValidado: any = {has_danger:'', form_control_danger:'', msj:''};
+  rutValidado: any = {has_danger:'', form_control_danger:'', msj:''};
+  correolValidado: any = {has_danger:'', form_control_danger:'', msj:''};
+  telefonoValidado: any = {has_danger:'', form_control_danger:'', msj:''};
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
  
  
 
  
   constructor(private titulo_navegador: Title) { }
-  validar(campo1: string, campo2: string, campo3: string, campo4:string, campo5: string){
-  
+  validacion(campo1: string, campo2: string, campo3: string, campo4:string, campo5: string){
+
     return (group: FormGroup) => {
-      let nombre = group.controls[campo1].value;
-      let apellido = group.controls[campo2].value;
-      let correo = group.controls[campo3].value;
-      let rut = group.controls[campo4].value;
-      let telefono = group.controls[campo5].value;
+      let nombre = group.controls[campo1];
+      let apellido = group.controls[campo2];
+      let correo = group.controls[campo3];
+      let rut = group.controls[campo4];
+      let telefono = group.controls[campo5];
      
-  
-      if(nombre){
-    
-        return null;
-    
-      }else{
+       
+      if(nombre.invalid){
+        // console.log(nombre);
+        this.nombresValidado.has_danger= 'form-group has-danger';
+        this.nombresValidado.form_control_danger= 'form-control form-control-danger';
+
+        if(nombre.errors.required)
+          this.nombresValidado.msj= 'El nombre es requerido';
         
-        this.nombresValidado.msj= 'El nombre es requerido';
-        this.nombresValidado.has_danger = `form-group has-danger`;
-        this.nombresValidado.form_control_danger = `form-control has-danger`;
+        if(nombre.errors.minlength)
+          this.nombresValidado.msj= 'El nombre es muy corto';
+          
+        if(nombre.errors.maxlength)
+          this.nombresValidado.msj= 'El nombre es muy largo';
+        
       }
+      if(apellido.invalid){
+        //console.log(apellido);
+        this.apellidosValidado.has_danger= 'form-group has-danger';
+        this.apellidosValidado.form_control_danger= 'form-control form-control-danger';
 
-      if(apellido){
-    
-        return null;
-    
-      }else{
-        this.apellidosValidado.display= 'block';
-        this.apellidosValidado.msj= 'El apellido es requerido';
-      }     
+        if(apellido.errors.required)
+          this.apellidosValidado.msj= 'El apellido es requerido';
+        
+        if(apellido.errors.minlength)
+          this.apellidosValidado.msj= 'El apellido es muy corto';
+          
+        if(apellido.errors.maxlength)
+          this.apellidosValidado.msj= 'El apellido es muy largo';
+        
+      }
+      if(rut.invalid){
+        //console.log(apellido);
+        this.rutValidado.has_danger= 'form-group has-danger';
+        this.rutValidado.form_control_danger= 'form-control form-control-danger';
 
-      if(correo){
-    
-        return null;
-    
-      }else{
-        this.correolValidado.display= 'block';
-        this.correolValidado.msj= 'El correo es requerido';
+        if(rut.errors.required)
+          this.rutValidado.msj= 'El rut es requerido';
+        
+        if(rut.errors.minlength)
+          this.rutValidado.msj= 'El rut es muy corto';
+          
+        if(rut.errors.maxlength)
+          this.rutValidado.msj= 'El rut es muy largo';
+        
       }
-      if(rut){
-    
-        return null;
-    
-      }else{
-        this.rutValidado.display= 'block';
-        this.rutValidado.msj= 'El rut es requerido';
-      }
-      if(telefono){
-    
-        return null;
-    
-      }else{
-        this.telefonoValidado.display= 'block';
-        this.telefonoValidado.msj= 'El telefono es requerido';
-      }
-     
+      
 
-      return {
-        validacion: true
-      }
+      return { validacion: true }
     }
   }
+  get nombres(){return this.forma.get('nombres')}
+  get apellidos(){return this.forma.get('apellidos')}
+  get rut(){return this.forma.get('rut')}
+  get telefono(){return this.forma.get('telefono')}
+  get correo(){return this.forma.get('correo')}
+
   ngOnInit() {
     this.titulo_navegador.setTitle('Cloud H & S | Pacientes');
     this.forma = new FormGroup({
       
-      nombres: new FormControl(null, Validators.required),
-      apellidos: new FormControl(null, Validators.required),
-      correo: new FormControl(null, [Validators.email]),
-      rut: new FormControl(null, Validators.required),
-      telefono: new FormControl(null, Validators.required)
-    }, this.validar('nombres','apellidos','correo','rut','telefono'));
+      nombres: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]),
+      apellidos: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]),
+      correo: new FormControl(null, [Validators.required, Validators.pattern(this.emailPattern)]),
+      rut: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(8)]),
+      telefono: new FormControl(null, [Validators.required])
+    }, this.validacion('nombres','apellidos','correo','rut','telefono'));
   }
+
+  
 
   
 
