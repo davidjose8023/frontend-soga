dac425dd8b7bdff1791b1e5bec3ba0c6031ce49b
David <davidjose8023@gmail.com>
2020-03-04 17:51:15 -0300

    cambios full calendar

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -11,22 +11,169 @@ File: js
 });*/
 
 function init_calendar() {
-    $('#calendar').fullCalendar({
-        editable: true,
-        height: 280,
-        displayEventTime : false,
-        views: {
-            month:{ buttonText: 'Calendario' },
-            listYear: { buttonText: 'Lista de Incidencias' }
+    !function($) {
+        "use strict";
+    
+        var CalendarApp = function() {
+            this.$body = $("body")
+            this.$calendar = $('#calendar'),
+            this.$event = ('#calendar-events div.calendar-events'),
+            this.$categoryForm = $('#add-new-event form'),
+            this.$extEvents = $('#calendar-events'),
+            this.$modal = $('#my-event'),
+            this.$saveCategoryBtn = $('.save-category'),
+            this.$calendarObj = null
+        };
+    
+    
+        
+        /* on click on event */
+        CalendarApp.prototype.onEventClick =  function (calEvent, jsEvent, view) {
+            var $this = this;
+                var form = $("<form></form>");
+                form.append("<label>Change event name</label>");
+                form.append("<div class='input-group'><input class='form-control' type=text value='" + calEvent.title + "' /><span class='input-group-btn'><button type='submit' class='btn btn-success waves-effect waves-light'><i class='fa fa-check'></i> Save</button></span></div>");
+                $this.$modal.modal({
+                    backdrop: 'static'
+                });
+                $this.$modal.find('.delete-event').show().end().find('.save-event').hide().end().find('.modal-body').empty().prepend(form).end().find('.delete-event').unbind('click').click(function () {
+                    $this.$calendarObj.fullCalendar('removeEvents', function (ev) {
+                        return (ev._id == calEvent._id);
+                    });
+                    $this.$modal.modal('hide');
+                });
+                $this.$modal.find('form').on('submit', function () {
+                    calEvent.title = form.find("input[type=text]").val();
+                    $this.$calendarObj.fullCalendar('updateEvent', calEvent);
+                    $this.$modal.modal('hide');
+                    return false;
+                });
         },
-        header: {
-            //language: 'es',
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,listYear'
 
+ 
+        CalendarApp.prototype.enableDrag = function() {
+            //init events
+            $(this.$event).each(function () {
+                // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+                // it doesn't need to have a start or end
+                var eventObject = {
+                    title: $.trim($(this).text()) // use the element's text as the event title
+                };
+                // store the Event Object in the DOM element so we can get to it later
+                $(this).data('eventObject', eventObject);
+                // make the event draggable using jQuery UI
+                $(this).draggable({
+                    zIndex: 999,
+                    revert: true,      // will cause the event to go back to its
+                    revertDuration: 0  //  original position after the drag
+                });
+            });
         }
-    });
+        /* Initializing */
+        CalendarApp.prototype.init = function() {
+            this.enableDrag();
+            /*  Initialize the calendar  */
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
+            var form = '';
+            var today = new Date($.now());
+    
+            var defaultEvents =  [{
+                    title: 'Released Ample Admin!',
+                    start: new Date($.now() + 506800000),
+                    className: 'bg-info'
+                }, {
+                    title: 'This is today check date',
+                    start: today,
+                    end: today,
+                    className: 'bg-danger'
+                }, {
+                    title: 'This is your birthday',
+                    start: new Date($.now() + 848000000),
+                    className: 'bg-info'
+                },{
+                    title: 'your meeting with john',
+                    start: new Date($.now() - 1099000000),
+                    end:  new Date($.now() - 919000000),
+                    className: 'bg-warning'
+                },{
+                    title: 'your meeting with john',
+                    start: new Date($.now() - 1199000000),
+                    end: new Date($.now() - 1199000000),
+                    className: 'bg-purple'
+                },{
+                    title: 'your meeting with john',
+                    start: new Date($.now() - 399000000),
+                    end: new Date($.now() - 219000000),
+                    className: 'bg-info'
+                },  
+                  {
+                    title: 'Hanns birthday',
+                    start: new Date($.now() + 868000000),
+                    className: 'bg-danger'
+                },{
+                    title: 'Like it?',
+                    start: new Date($.now() + 348000000),
+                    className: 'bg-success'
+                }];
+    
+            var $this = this;
+            $this.$calendarObj = $this.$calendar.fullCalendar({
+                //slotDuration: '00:00:02', /* If we want to split day time each 15minutes */
+                monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+                monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+                minTime: '08:00:00',
+                maxTime: '19:00:00',  
+                defaultView: 'month',  
+                //handleWindowResize: true,  
+                
+                views: {
+                    month:{ buttonText: 'Calendario' },
+                    agendaDay: { buttonText: 'Lista dpor día' }
+                },
+                 
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    //right: 'month,agendaWeek,agendaDay'
+                    right: 'month,agendaDay'
+                },
+                events: defaultEvents,
+                editable: true,
+                droppable: true, // this allows things to be dropped onto the calendar !!!
+                eventLimit: true, // allow "more" link when too many events
+                eventClick: function(calEvent, jsEvent, view) { $this.onEventClick(calEvent, jsEvent, view); }
+     
+    
+            });
+    
+            //on new event
+            this.$saveCategoryBtn.on('click', function(){
+                var categoryName = $this.$categoryForm.find("input[name='category-name']").val();
+                var categoryColor = $this.$categoryForm.find("select[name='category-color']").val();
+                if (categoryName !== null && categoryName.length != 0) {
+                    $this.$extEvents.append('<div class="calendar-events" data-class="bg-' + categoryColor + '" style="position: relative;"><i class="fa fa-circle text-' + categoryColor + '"></i>' + categoryName + '</div>')
+                    $this.enableDrag();
+                }
+    
+            });
+        },
+    
+       //init CalendarApp
+        $.CalendarApp = new CalendarApp, $.CalendarApp.Constructor = CalendarApp
+        
+    }(window.jQuery),
+    
+    //initializing CalendarApp
+    function($) {
+        "use strict";
+        $.CalendarApp.init()
+        
+    }(window.jQuery);
+
+    
 
 
 }
